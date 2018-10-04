4446769c11124641a2a6cbb3f222d8b93dd20d1c
Azumi Callanan <azumi@Azumis-MacBook.local>
2018-10-04 09:52:09 -0700

    CP Basic Node Operations: In progress

--- commands.js
+++ commands.js
@@ -1,44 +1,61 @@
 const fs = require("fs");
 
-//write out data
  function done(output) {
      process.stdout.write(output);
      process.stdout.write('\nprompt > ');
  }
 
-//where we will store our commands
  function evaluateCmd(userInput) {
-  //parses the user input to understand which command was typed
-  //i.e. ["cd", "main.js"]=>userInputArray[0]="cd"
    const userInputArray = userInput.split(" ");
    const command = userInputArray[0];
 
    switch (command) {
      case "echo":
-      //we will add the functionality of echo next within the object commandLibrary
-      //slice(1) returns array items except "echo"
        commandLibrary.echo(userInputArray.slice(1).join(" "));
        break;
      case "cat":
        commandLibrary.cat(userInputArray.slice(1));
        break;
+     case "head":
+       commandLibrary.head(userInputArray.slice(1));
+       break;
+     case "tail":
+       commandLibrary.tail(userInputArray.slice(1));
+       break;
+     default:
+       console.log("command cannot be found")
    }
  }
 
-//where we will store the logic of our commands
  const commandLibrary = {
-    //the echo command
     "echo": function(userInput) {
         done(userInput);
     },
-    //the cat command
     "cat": function(fullPath) {
       const fileName = fullPath[0];
       fs.readFile(fileName, (err, data) => {
           if (err) throw err;
           done(data);
       });
-  }
+    },
+    "head": function(fullPath) {
+      const fileName = fullPath[0];
+      fs.readFile(fileName, (err, data, n) => {
+          if (err) throw err;
+          let splitArray = data.split("\n");
+          let firstNLines = splitArray.slice(0,n).join("\n");
+          done(firstNLines);
+      });
+    },
+    "tail": function(fullPath) {
+      const fileName = fullPath[0];
+      fs.readFile(fileName, (err, data, n) => {
+          if (err) throw err;
+          let splitArray = data.split('\n');
+          let lastNLines = splitArray.slice(-n).join('\n');
+          done(data);
+      });
+    },
  };
 
  module.exports.commandLibrary = commandLibrary;
